860d46ff815851982e813a033fd8a16c38e575ba
Allison Randal <allison@perl.org>
2005-11-09 07:55:24 +0000

    r575@lilal (orig r10):  allison | 2003-11-30 16:48:20 -0800  Added event handler methods to convert events into method calls. Updated documentation.

--- lib/Pod/PseudoPod.pm
+++ lib/Pod/PseudoPod.pm
@@ -27,6 +27,30 @@ sub new {
   return $new;
 }
 
+sub _handle_element_start {
+  my ($self, $element, $arg) = @_;
+
+  $element =~ tr/-:./__/;
+
+  my $sub = $self->can('start_' . $element);
+  $sub->($self,$arg) if $sub; 
+}
+
+sub _handle_text {
+  my $self = shift;
+
+  my $sub = $self->can('handle_text');
+  $sub->($self, @_) if $sub;
+}
+
+sub _handle_element_end {
+  my ($self,$element) = @_;
+  $element =~ tr/-:./__/;
+
+  my $sub = $self->can('end_' . $element);
+  $sub->($self) if $sub;
+}
+
 1; 
 
 __END__
@@ -37,70 +61,63 @@ Pod::PseudoPod - A framework for parsing PseudoPOD
 
 =head1 SYNOPSIS
 
-  use Pod::PseudoPod
-  blah blah blah
+  use strict;
+  package SomePseudoPodFormatter;
+  use base qw(Pod::PseudoPod);
+
+  sub handle_text {
+    my($self, $text) = @_;
+    ...
+  }
+
+  sub start_head1 {
+    my($self, $attrs) = @_;
+    ...
+  }
+  sub end_head1 {
+    my($self) = @_;
+   ...
+  }
+
+  ...and start_*/end_* methods for whatever other events you
+  want to catch.
 
 
 =head1 DESCRIPTION
 
-PseudoPOD is O'Reilly's extended set of POD tags for book manuscripts. Standard
-POD doesn't have all the markup options you might want for marking up files for
-publishing production. PseudoPOD adds a few extra tags for footnotes, etc.
+PseudoPOD is O'Reilly's extended set of POD tags for book manuscripts.
+Standard POD doesn't have all the markup options you might want for
+marking up files for publishing production. PseudoPOD adds a few extra
+tags for footnotes, etc.
 
+This class adds parsing support for the PseudoPOD tags. It also
+overrides Pod::Simple's C<_handle_element_start>, C<_handle_text>, and
+C<_handle_element_end> methods so that parser events are turned into
+method calls. (Otherwise, this is a subclass of Pod::Simple and
+inherits all its methods.)
 
-=head1 USAGE
-
-
-
-=head1 BUGS
-
-
-
-=head1 SUPPORT
-
-
-
-=head1 AUTHOR
-
-	Allison Randal <al@shadowed.net>
-	http://www.onyxneon.com
-
-=head1 COPYRIGHT
-
-This program is free software; you can redistribute
-it and/or modify it under the same terms as Perl itself.
-
-The full text of the license can be found in the
-LICENSE file included with this module.
-
+You can use this class as the base class for a PseudoPod
+formatter/processor.
 
 =head1 SEE ALSO
 
-perl(1).
+L<Pod::Simple>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2003 Allison Randal.  All rights reserved.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself. The full text of the license
+can be found in the LICENSE file included with this module.
+
+This library is distributed in the hope that it will be useful, but
+without any warranty; without even the implied warranty of
+merchantability or fitness for a particular purpose.
+
+=head1 AUTHOR
+
+Allison Randal C<allison@cpan.org>
 
 =cut
-
-############################################# main pod documentation end ##
-
-
-################################################ subroutine header begin ##
-
-=head2 sample_function
-
- Usage     : How to use this function/method
- Purpose   : What it does
- Returns   : What it returns
- Argument  : What it wants to know
- Throws    : Exceptions and other anomolies
- Comments  : This is a sample subroutine header.
-           : It is polite to include more pod and fewer comments.
-
-See Also   : 
-
-=cut
-
-################################################## subroutine header end ##
-
-
-
 
