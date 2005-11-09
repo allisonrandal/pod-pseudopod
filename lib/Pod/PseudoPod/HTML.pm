67a16713cbcb6723afa4f25c7a3154ffb25613ae
Allison Randal <allison@perl.org>
2005-11-09 07:55:19 +0000

    r574@lilal (orig r9):  allison | 2003-11-30 16:27:15 -0800  Switching Pod::PseudoPod::HTML over to a method based format. Much easier to work with.

--- lib/Pod/PseudoPod/HTML.pm
+++ lib/Pod/PseudoPod/HTML.pm
@@ -1,145 +1,125 @@
+
+require 5;
 package Pod::PseudoPod::HTML;
 use strict;
+use Carp ();
 use Pod::PseudoPod ();
-use vars qw(@ISA $VERSION %tags);
-$VERSION = 1.02;
+use vars qw( @ISA $VERSION );
+$VERSION = '1.02';
 @ISA = ('Pod::PseudoPod');
+BEGIN { *DEBUG = defined(&Pod::PseudoPod::DEBUG)
+          ? \&Pod::PseudoPod::DEBUG
+          : sub() {0}
+      }
 
-%tags = (
-    'Para' => {
-        'start' => '<p>',
-        'end'   => '</p>',
-    },
-    'Verbatim' => {
-        'start' => '<pre>',
-        'end'   => '</pre>',
-    },
-    'head0' => {
-        'start' => '<h1>',
-        'end'   => '</h1>',
-    },
-    'head1' => {
-        'start' => '<h1>',
-        'end'   => '</h1>',
-    },
-    'head2' => {
-        'start' => '<h2>',
-        'end'   => '</h2>',
-    },
-    'head3' => {
-        'start' => '<h3>',
-        'end'   => '</h3>',
-    },
-    'head4' => {
-        'start' => '<h4>',
-        'end'   => '</h4>',
-    },
-    'over_bullet' => {
-        'start' => '<ul>',
-        'end'   => '</ul>',
-    },
-);
+use Text::Wrap 98.112902 ();
+$Text::Wrap::wrap = 'overflow';
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-sub _handle_element_start {
-    my ($self,$element,$thirdarg) = @_;
-
-    $element =~ tr/-:./__/;
-
-	my $sub = $self->can( 'start_' . $element );
-	$sub->($self,$thirdarg) if $sub; 
+sub new {
+  my $self = shift;
+  my $new = $self->SUPER::new(@_);
+  $new->{'output_fh'} ||= *STDOUT{IO};
+  $new->accept_target_as_text(qw( text plaintext plain ));
+#  $new->nix_X_codes(1);
+  $new->nbsp_for_S(1);
+  $new->{'Thispara'} = '';
+  $new->{'Indent'} = 0;
+  $new->{'Indentstring'} = '   ';
+  return $new;
 }
 
-sub _handle_text {
-    my ($self,$text) = @_;
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    $self->{'buffer'} .= $text;
-}
+sub handle_text { $_[0]{'Thispara'} .= $_[1] }
 
-sub _handle_element_end {
-    my ($self,$element,$thirdarg) = @_;
+sub start_Para     { $_[0]{'Thispara'} = '<p>' }
+sub start_Verbatim { $_[0]{'Thispara'} = '<pre><code>' }
 
-    $element =~ tr/-:./__/;
-	my $sub = $self->can( 'start_' . $element );
-	$sub->($self) if $sub; 
+sub start_head0 {  $_[0]{'Thispara'} = '<h1>' }
+sub start_head1 {  $_[0]{'Thispara'} = '<h2>' }
+sub start_head2 {  $_[0]{'Thispara'} = '<h3>' }
+sub start_head3 {  $_[0]{'Thispara'} = '<h4>' }
+sub start_head4 {  $_[0]{'Thispara'} = '<h5>' }
 
+sub start_item_bullet { $_[0]{'Thispara'} = '<li>' }
+sub start_item_number { $_[0]{'Thispara'} = "<li>$_[1]{'number'}. "  }
+sub start_item_text   { $_[0]{'Thispara'} = '<li>'   }
+
+sub start_over_bullet { $_[0]{'Thispara'} = '<ul>'; $_[0]->emit_par() }
+sub start_over_text   { $_[0]{'Thispara'} = '<ul>'; $_[0]->emit_par() }
+sub start_over_block  { $_[0]{'Thispara'} = '<ul>'; $_[0]->emit_par() }
+sub start_over_number { $_[0]{'Thispara'} = '<ol>'; $_[0]->emit_par() }
+
+sub end_over_bullet { $_[0]{'Thispara'} .= '</ul>'; $_[0]->emit_par() }
+sub end_over_text   { $_[0]{'Thispara'} .= '</ul>'; $_[0]->emit_par() }
+sub end_over_block  { $_[0]{'Thispara'} .= '</ul>'; $_[0]->emit_par() }
+sub end_over_number { $_[0]{'Thispara'} .= '</ol>'; $_[0]->emit_par() }
+
+# . . . . . Now the actual formatters:
+
+sub end_Para     { $_[0]{'Thispara'} .= '</p>'; $_[0]->emit_par() }
+sub end_Verbatim { $_[0]{'Thispara'} .= '</code></pre>'; $_[0]->emit_par('nowrap') }
+
+sub end_head0       { $_[0]{'Thispara'} .= '</h1>'; $_[0]->emit_par() }
+sub end_head1       { $_[0]{'Thispara'} .= '</h2>'; $_[0]->emit_par() }
+sub end_head2       { $_[0]{'Thispara'} .= '</h3>'; $_[0]->emit_par() }
+sub end_head3       { $_[0]{'Thispara'} .= '</h4>'; $_[0]->emit_par() }
+sub end_head4       { $_[0]{'Thispara'} .= '</h5>'; $_[0]->emit_par() }
+
+sub end_item_bullet { $_[0]{'Thispara'} .= '</li>'; $_[0]->emit_par() }
+sub end_item_number { $_[0]{'Thispara'} .= '</li>'; $_[0]->emit_par() }
+sub end_item_text   { $_[0]->emit_par() }
+
+sub emit_par {
+  my($self, $nowrap) = @_;
+
+  my $out = $self->{'Thispara'} . "\n";
+
+  $out = Text::Wrap::wrap('', '', $out) unless $nowrap;
+
+  if(Pod::PseudoPod::ASCII) {
+    $out =~ tr{\xA0}{ };
+    $out =~ tr{\xAD}{}d;
+  }
+
+  print {$self->{'output_fh'}} $out, "\n";
+  $self->{'Thispara'} = '';
+  
+  return;
 }
 
 1;
-
 
 __END__
 
 =head1 NAME
 
-Pod::PseudoPod::Methody -- turn Pod::Simple events into method calls
+Pod::PseudoPod::HTML -- format PseudoPod as HTML
 
 =head1 SYNOPSIS
 
- require 5;
- use strict;
- package SomePodFormatter;
- use base qw(Pod::PseudoPod::Methody);
- 
- sub handle_text {
-   my($self, $text) = @_;
-   ...
- }
- 
- sub start_head1 {
-   my($self, $attrs) = @_;
-   ...
- }
- sub end_head1 {
-   my($self) = @_;
-   ...
- }
- 
-...and start_/end_ methods for whatever other events you want to catch.
+  perl -MPod::PseudoPod::HTML -e \
+   "exit Pod::PseudoPod::HTML->filter(shift)->any_errata_seen" \
+   thingy.pod
 
 =head1 DESCRIPTION
 
-This class is a straight copy of Pod::Simple with the inheritance tree 
-twisted a little. I've left the remainder of the documentation untouched 
-from the original, but you probably want to look at Pod::Simple::Methody.
+This class is a formatter that takes Pod and renders it as
+wrapped html.
 
-This class (which is very small -- read the source) overrides
-Pod::Simple's _handle_element_start, _handle_text, and
-_handle_element_end methods so that parser events are turned into method
-calls. (Otherwise, this is a subclass of L<Pod::Simple> and inherits all
-its methods.)
+Its wrapping is done by L<Text::Wrap>, so you can change
+C<$Text::Wrap::columns> as you like.
 
-You can use this class as the base class for a Pod formatter/processor.
-
-=head1 METHOD CALLING
-
-When Pod::Simple sees a "=head1 Hi there", for example, it basically does
-this:
-
-  $parser->_handle_element_start( "head1", \%attributes );
-  $parser->_handle_text( "Hi there" );
-  $parser->_handle_element_end( "head1" );
-
-But if you subclass Pod::Simple::Methody, it will instead do this
-when it sees a "=head1 Hi there":
-
-  $parser->start_head1( \%attributes ) if $parser->can('start_head1');
-  $parser->handle_text( "Hi there" )   if $parser->can('handle_text');
-  $parser->end_head1()                 if $parser->can('end_head1');
-
-If Pod::Simple sends an event where the element name has a dash,
-period, or colon, the corresponding method name will have a underscore
-in its place.  For example, "foo.bar:baz" becomes start_foo_bar_baz
-and end_foo_bar_baz.
-
-See the source for Pod::Simple::Text for an example of using this class.
+This is a subclass of L<Pod::PseudoPod> and inherits all its methods.
 
 =head1 SEE ALSO
 
-L<Pod::Simple>, L<Pod::Simple::Subclassing>
+L<Pod::PseudoPod>, L<Pod::Simple>
 
 =head1 COPYRIGHT AND DISCLAIMERS
 
-Copyright (c) 2002 Sean M. Burke.  All rights reserved.
+Copyright (c) 2003 Allison Randal.  All rights reserved.
 
 This library is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
@@ -150,7 +130,7 @@ merchantability or fitness for a particular purpose.
 
 =head1 AUTHOR
 
-Sean M. Burke C<sburke@cpan.org>
+Allison Randal C<allison@cpan.org>
 
 =cut
 
