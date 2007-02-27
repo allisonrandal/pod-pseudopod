ede86c96ebd23f47d1caeec43f73aced696c5433
Allison Randal <allison@perl.org>
2007-02-27 15:26:44 +0000

    Results of testing on various sample manuscripts.

--- lib/Pod/PseudoPod/DocBook.pm
+++ lib/Pod/PseudoPod/DocBook.pm
@@ -14,34 +14,38 @@ sub new {
   my $new = $self->SUPER::new(@_);
   $new->{'output_fh'} ||= *STDOUT{IO};
   $new->accept_targets( 'docbook', 'DocBook' );
-  $new->accept_targets_as_text( qw(author blockquote comment caution
+  $new->accept_targets_as_text( qw(author blockquote caution
       editor epigraph example figure important literal note
       production screen sidebar table tip warning) );
 
-  $new->nix_X_codes(1);
+  $new->nix_Z_codes(1);
   $new->nbsp_for_S(1);
   $new->codes_in_verbatim(1);
+  $new->chapter_type('chapter'); # default chapter type
   $new->{'scratch'} = '';
   $new->{'sections'} = (); # a stack for tracking section nesting
-  $new->{'sectionname'} = {
-      0 => 'chapter',
-      1 => 'sect1',
-      2 => 'sect2',
-      3 => 'sect3',
-      4 => 'sect4',
-  };
+  $new->{'sectionnum'} = (); # a list for tracking section number
+  $new->{'sectionname'} = ['chapter','sect1', 'sect2', 'sect3', 'sect4'];
   return $new;
 }
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+#sub handle_text {
+    # escape special characters in DocBook (<, >, &, etc)
+#    $_[0]{'scratch'} .= $_[0]{'in_verbatim'} ? encode_entities( $_[1] ) : $_[1]
+#}
+
 sub handle_text {
     # escape special characters in DocBook (<, >, &, etc)
-    $_[0]{'scratch'} .= $_[0]{'in_verbatim'} ? encode_entities( $_[1] ) : $_[1]
+    $_[0]{'scratch'} .= encode_entities( $_[1] );
 }
 
 sub start_Para     { $_[0]{'scratch'} = '<para>' unless $_[0]{'in_figure'} }
-sub start_Verbatim { $_[0]{'scratch'} = "<programlisting>\n"; $_[0]{'in_verbatim'} = 1}
+sub start_Verbatim {
+  $_[0]{'scratch'} = "<programlisting>\n" unless ($_[0]{'in_screen'});
+  $_[0]{'in_verbatim'} = 1;
+}
 
 sub start_head0 { $_[0]->set_section(0); }
 sub start_head1 { $_[0]->set_section(1); }
@@ -52,7 +56,18 @@ sub start_head4 { $_[0]->set_section(4); }
 sub set_section {
     my ($self, $level) = @_;
     $self->{'scratch'} = $self->close_sections($level);
-    $self->{'scratch'} .= "<" . $self->{'sectionname'}{$level} . ' id="" label="" role="">';
+    $self->{'sectionnum'}[$level]++ if ($level > 0);
+    my $label = join '.', @{$self->{'sectionnum'}};
+    my $id = $self->chapter_id();
+    if ($level > 0) {
+      $id .= '-SECT-';
+      my @sectionnum = @{$self->{'sectionnum'}};
+      $id .= join '.', @sectionnum[1 ..  $#sectionnum];
+    }
+    $self->{'scratch'} .= '<' . $self->{'sectionname'}[$level];
+    $self->{'scratch'} .= ' id="' . $id;
+    $self->{'scratch'} .= '" label="' . $label unless $self->{'chapter_type'} eq 'preface';
+    $self->{'scratch'} .= '" role="">';
     $self->{'scratch'} .= "\n<title>";
     push @{$self->{'sections'}}, $level;
 }
@@ -66,7 +81,8 @@ sub close_sections {
             && @$sections > 0
             && $level <= $sections->[-1]) {
         my $closing = pop @$sections;
-        $scratch .= "</" . $self->{'sectionname'}{$closing} . ">\n";
+        delete $self->{'sectionnum'}[$closing + 1];
+        $scratch .= "</" . $self->{'sectionname'}[$closing] . ">\n";
     }
     return $scratch;
 }
@@ -78,19 +94,30 @@ sub start_over_text   { $_[0]{'scratch'} = '<variablelist>'; $_[0]->emit() }
 
 sub end_over_bullet { $_[0]{'scratch'} .= '</itemizedlist>'; $_[0]->emit() }
 sub end_over_block  { $_[0]{'scratch'} .= '</itemizedlist>'; $_[0]->emit() }
-sub end_over_number { $_[0]{'scratch'} .= '</orderedlist>'; $_[0]->emit() }
+sub end_over_number {
+  $_[0]{'scratch'} .= "</para></listitem>\n" if ($_[0]{'in_numlist'});
+  $_[0]{'scratch'} .= '</orderedlist>';
+  $_[0]{'in_numlist'} = 0;
+  $_[0]->emit();
+}
 sub end_over_text   { 
   $_[0]{'scratch'} .= "</listitem>\n</varlistentry>\n" if ($_[0]{'in_varlist'});
-  $_[0]{'in_varlist'} = 0;
   $_[0]{'scratch'} .= '</variablelist>';
+  $_[0]{'in_varlist'} = 0;
   $_[0]->emit();
 }
 
-sub start_item_bullet { $_[0]{'scratch'} = '<listitem>' }
-sub start_item_number { $_[0]{'scratch'} = "<listitem><para>"  }
+sub start_item_bullet { $_[0]{'scratch'} = '<listitem><para>' }
+sub end_item_bullet { $_[0]{'scratch'} .= '</para></listitem>'; $_[0]->emit() }
 
-sub end_item_bullet { $_[0]{'scratch'} .= '</listitem>'; $_[0]->emit() }
-sub end_item_number { $_[0]{'scratch'} .= '</para></listitem>'; $_[0]->emit() }
+sub start_item_number { 
+  $_[0]{'scratch'} .= "</para></listitem>\n" if ($_[0]{'in_numlist'});
+  $_[0]{'scratch'} .= "<listitem><para>";
+  $_[0]{'in_numlist'} = 1;
+}
+sub end_item_number {
+  $_[0]->emit()
+}
 
 sub start_item_text   { 
     $_[0]{'scratch'} .= "</listitem>\n</varlistentry>\n" if ($_[0]{'in_varlist'});
@@ -113,7 +140,7 @@ sub end_Para {
     }
 }
 sub end_Verbatim {
-    $_[0]{'scratch'}     .= "\n</programlisting>";
+    $_[0]{'scratch'}     .= "\n</programlisting>" unless ($_[0]{'in_screen'});
     $_[0]{'in_verbatim'}  = 0;
     $_[0]->emit();
 }
@@ -157,26 +184,34 @@ sub end_figure {
 # This handles =begin and =for blocks of all kinds.
 sub start_for { 
     my ($self, $flags) = @_;
-    $self->{'scratch'} .= '<'.$flags->{'target'}.'>';
+    my $target = $flags->{'target'};
+    $self->{'scratch'} .= "<$target>";
+    $self->{"in_$target"} = 1;
     $self->emit();
 
 }
 sub end_for { 
     my ($self, $flags) = @_;
-    $self->{'scratch'} .= '</'.$flags->{'target'}.'>';
+    my $target = $flags->{'target'};
+    $self->{'scratch'} .= "</$target>";
+    $self->{"in_$target"} = 0;
     $self->emit();
 }
 
 sub start_table { 
   my ($self, $flags) = @_;
-  $self->{'scratch'} .= '<table id="" label="" frame="topbot" colsep="0" rowsep="0">';
+  my $id = $self->chapter_id() . '-TABLE-'. $self->table_next();
+  $self->{'scratch'} .= '<table id="'.$id.'" label="" frame="topbot" ';
+  $self->{'scratch'} .= 'colsep="0" rowsep="0">';
   if ($flags->{'title'}) {
-    $self->{'scratch'} .= '<title>' . $flags->{'title'} . '</title>';
+    $self->{'scratch'} .= "\n<title>" . $flags->{'title'} . '</title>';
   }
+  $self->{'scratch'} .= "\n" . '<tgroup cols="">';
   $self->emit();
 }
+sub table_next { ++$_[0]{'table_count'} }
 
-sub end_table   { $_[0]{'scratch'} .= '</tbody></table>'; $_[0]->emit() }
+sub end_table   { $_[0]{'scratch'} .= '</tbody></tgroup></table>'; $_[0]->emit() }
 
 sub start_headrow { $_[0]{'scratch'} .= "<thead>\n"; $_[0]{'headrow'} = 1 }
 sub start_bodyrows {
@@ -186,10 +221,12 @@ sub start_bodyrows {
     $self->{'scratch'} .= "<tbody>\n";
 }
 
-sub start_row { $_[0]{'scratch'} .= "<row>\n" }
+sub start_row {$_[0]{'scratch'} .= "<row>\n" }
 sub end_row { $_[0]{'scratch'} .= '</row>'; $_[0]->emit() }
 
-sub start_cell { $_[0]{'scratch'} .= '<entry align="left"><para>'; }
+sub start_cell {
+  $_[0]{'scratch'} .= '<entry align="left"><para>';
+}
 sub end_cell { 
   my $self = shift;
   $self->{'scratch'} .= '</para></entry>';
@@ -232,12 +269,14 @@ sub end_I   { $_[0]{'scratch'} .= '</emphasis>' }
 
 sub start_N {
   my ($self) = @_;
-  $self->{'scratch'} .= '<footnote id="" label="*"><para>';
+  my $id = $self->chapter_id() . '-FNOTE-'. $self->footnote_next();
+  $self->{'scratch'} .= '<footnote id="'.$id.'" label="*"><para>';
 }
 sub end_N {
   my ($self) = @_;
   $self->{'scratch'} .= '</para></footnote>';
 }
+sub footnote_next { ++$_[0]{'footnote_count'} }
 
 sub start_R { $_[0]{'scratch'} .= '<replaceable>' }
 sub end_R   { $_[0]{'scratch'} .= '</replaceable>' }
@@ -245,11 +284,17 @@ sub end_R   { $_[0]{'scratch'} .= '</replaceable>' }
 sub start_U { $_[0]{'scratch'} .= '<systemitem role="url">' }
 sub end_U   { $_[0]{'scratch'} .= '</systemitem>' }
 
-sub start_X { $_[0]{'scratch'} .= '<indexterm id="">' }
-sub end_X   { $_[0]{'scratch'} .= '</indexterm>' }
-
-sub start_Z { $_[0]{'scratch'} .= '<a name="' }
-sub end_Z   { $_[0]{'scratch'} .= '">' }
+sub start_X {
+  my ($self) = @_;
+  my $id = $self->chapter_id() . '-IDX-' . $self->index_next();
+  $self->{'scratch'} .= '<indexterm id="'.$id.'"><primary>';
+}
+sub end_X   { $_[0]{'scratch'} .= '</primary></indexterm>' }
+sub index_next { 
+  my ($self) = @_;
+  my $idx = ++$self->{'index_count'}; 
+  return sprintf("%04d", $idx);
+}
 
 sub emit {
   my($self) = @_;
@@ -259,6 +304,40 @@ sub emit {
       $self->{'scratch'} = '';
   }
   return;
+}
+
+sub book_id { $_[0]{'book_id'} = $_[1] }
+sub index_count {
+  $_[0]{'index_count'} = $_[1] if ($_[1]);
+  return $_[0]{'index_count'};
+}
+sub chapter_num { 
+    my ($self, $number) = @_;
+    $self->{'chapter_num'} = $number;
+    $self->{'sectionnum'}[0] = $number;
+}
+sub chapter_type { 
+    my ($self, $type) = @_;
+    $self->{'chapter_type'} = $type;
+    $self->{'sectionname'}[0] = $type;
+}
+sub chapter_id {
+    my ($self) = @_;
+    unless ($self->{'chapter_id'}) {
+      my $id;
+      $id = $self->{'book_id'} . '-' if ($self->{'book_id'});
+      if ($self->{'chapter_type'} eq 'preface') {
+        $id .= 'PREFACE';
+      } elsif ($self->{'chapter_type'} eq 'colophon') {
+        $id .= 'COLOPHON';
+      } elsif ($self->{'chapter_type'} eq 'appendix') {
+        $id .= 'APP-' . $self->{'chapter_num'};
+      } elsif ($self->{'chapter_type'} eq 'chapter') {
+        $id .= 'CHP-' . $self->{'chapter_num'};
+      }
+      $self->{'chapter_id'} = $id;
+    }
+    return $self->{'chapter_id'};
 }
 
 # bypass built-in E<> handling to preserve entity encoding
@@ -281,6 +360,11 @@ Pod::PseudoPod::DocBook -- format PseudoPod as DocBook
   ...
 
   $parser->parse_file('path/to/file.pod');
+
+Before sending in your manuscript, check that the formatter produced a
+well-formed DocBook file with I<xmllint>:
+
+  $ xmllint --noout --valid book.xml
 
 =head1 DESCRIPTION
 
